a35cd119e3561bb1f5fcf2735873f287a46cd08c
Obberton13 <ryoung.david@gmail.com>
2015-02-26 04:32:32 -0600

    Added javadocs for the store controller, started work on the main class

--- src/MediaRental/Controller/Store.java
+++ src/MediaRental/Controller/Store.java
@@ -10,47 +10,91 @@ import java.util.HashMap;
  * Created by Obberton13 on 2/16/2015.
  */
 public class Store {
+
+	/**
+	 * @param name the name of the customer to add
+	 * @param address the address of the customer to add
+	 * @return true on success, false otherwise
+	 */
 	public boolean AddCustomer(String name, String address) {
 		MediaRental.Model.Store store = this.getStore();
 		Customer customer = new Customer();
 		customer.setStore(store);
 		customer.setName(name);
 		customer.setAddress(address);
-		return DatabaseSupport.putCustomer(customer);
+		return false;
+		//return DatabaseSupport.putCustomer(customer);
 	}
 
+	/**
+	 * @param cid the ID of the customer to remove
+	 * @return true on success, false otherwise
+	 */
 	public boolean RemoveCustomer(int cid) {
-		return DatabaseSupport.removeCustomer(cid);
+		return false;
+		//return DatabaseSupport.removeCustomer(cid);
 	}
 
-	public boolean CreateProduct(String name, String type, ArrayList<String> keywords) {
+	/**
+	 * @param name the name of the product to create
+	 * @param type the type of the product to create
+	 * @return true on success, false otherwise
+	 */
+	public boolean CreateProduct(String name, String type) {
 		Product product = new Product();
 		product.setStore(this.getStore());
 		product.setTitle(name);
 		product.setQuantity(0);
 		product.setType(type);
-		return DatabaseSupport.putProduct(product);
+		return false;
+		//return DatabaseSupport.putProduct(product);
 	}
 
+	/**
+	 * @param pid the ID of the product to add to the store
+	 * @param qty the amount of the product to add to the store
+	 * @return true on success, false otherwise
+	 */
 	public boolean AddProduct(int pid, int qty) {
 		return true;
 	}
 
+	/**
+	 * @param cid The ID of the customer who is doing the purchasing
+	 * @return true on success, false otherwise
+	 */
 	public boolean CreateTransaction(int cid){
 		return true;
 	}
 
+	/**
+	 * @param productID The ID of the product to add to the transaction
+	 * @param transactionID The ID of the transaction to be added to
+	 * @return true on success, false otherwise
+	 */
 	public boolean AddSale(int productID, int transactionID) {
 		return true;
 	}
 
-	public boolean AddRental(int productID, int transactionID, int rentalLenght) {
+	/**
+	 * @param productID The ID of the product to add to the transaction
+	 * @param transactionID The ID of the transaction to be added to
+	 * @param rentalLength The length that the specified product will be rented
+	 * @return true on success, false otherwise
+	 */
+	public boolean AddRental(int productID, int transactionID, int rentalLength) {
 		return true;
 	}
 
+	/**
+	 * @param arguments The arguments used to find the product(s).
+	 * @return an ArrayList of products containing all products found that match the arguments
+	 */
 	public ArrayList<Product> FindProduct(HashMap<String, String> arguments) {
 		return new ArrayList<Product>();
 	}
+
+	//private helper methods
 	private MediaRental.Model.Store getStore()
 	{
 		return new MediaRental.Model.Store();
--- src/MediaRental/mainClass.java
+++ src/MediaRental/mainClass.java
@@ -1,11 +1,23 @@
 package MediaRental;
 
+import java.util.Scanner;
+
 /**
  * Created by Obberton13 on 2/20/2015.
  */
 public class mainClass {
 	public static void main(String args[])
 	{
+		System.out.println("Hey, it did something!");
+		Scanner sc = new Scanner(System.in);
 
+		//this is the main loop for the program.
+		while(sc.hasNext())
+		{
+			String line = sc.nextLine();
+			System.out.println("Hey, it scanned something!" + line);
+			if(line.equals("quit")) break;
+		}
+		sc.close();
 	}
 }
