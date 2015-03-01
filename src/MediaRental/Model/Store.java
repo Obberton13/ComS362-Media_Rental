e0a7c67a439907aa8a534ce0d6e944a0b31b2d51
Robert Young <ryoung.david@gmail.com>
2015-03-01 16:09:49 -0600

    Revert "Removed the Store Model. It will not be used."
    This reverts commit 75437e2f6b8e5da79952ffbd36a4fd727fbf38ed.

---
+++ src/MediaRental/Model/Store.java
@@ -0,0 +1,54 @@
+package MediaRental.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Obberton13 on 2/16/2015.
+ */
+public class Store {
+	protected int id;
+	protected String name;
+	protected ArrayList<Transaction> transactions;
+	protected ArrayList<Customer> customers;
+	protected ArrayList<Product> products;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ArrayList<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(ArrayList<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
+	public ArrayList<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
+
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+}
