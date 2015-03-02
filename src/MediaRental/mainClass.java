2a8640f488baa84fb5ef6df700952c69bb0edde4
Obberton13 <ryoung.david@gmail.com>
2015-03-01 21:34:03 -0600

    Finished the Product Index

--- src/MediaRental/mainClass.java
+++ src/MediaRental/mainClass.java
@@ -2,36 +2,54 @@ package MediaRental;
 
 import java.util.HashMap;
 import java.util.Scanner;
+
 import MediaRental.Controller.*;
 import MediaRental.Model.Product;
 
 /**
  * Created by Obberton13 on 2/20/2015.
  */
-public class mainClass {
+public class mainClass
+{
 	public static void main(String args[])
 	{
 		//this is the main loop for the program.
 		Scanner sc = new Scanner(System.in);
-		while(sc.hasNext())
+		while (sc.hasNext())
 		{
 			String in = sc.next().toLowerCase();
 			char c = in.charAt(0);
-			switch(c)
+			switch (c)
 			{
-				case 'q': break;
-				case 'a': Create(sc); break;
-				case 'e': Edit(sc); break;
-				case 'i': Index(sc); break;
-				case 'd': Delete(sc); break;
-				case 'c': BaseCommands(); break;
-				default: System.out.println("Invalid command: " + in);
+				case 'q':
+					break;
+				case 'a':
+					Create(sc);
+					break;
+				case 'e':
+					Edit(sc);
+					break;
+				case 'i':
+					Index(sc);
+					break;
+				case 'd':
+					Delete(sc);
+					break;
+				case 'c':
+					BaseCommands();
+					break;
+				default:
+					System.out.println("Invalid command: " + in);
 			}
-			if(c=='q') break;
+			if (c == 'q')
+			{
+				break;
+			}
 		}
 		System.out.println("G'bye!");
 		sc.close();
 	}
+
 	private static void Create(Scanner in)
 	{
 		System.out.println("<c|p|t|q>\n\n" +
@@ -40,7 +58,7 @@ public class mainClass {
 				"t adds a transaction\n" +
 				"q returns to the previous menu");
 		String input = in.next();
-		switch(input.charAt(0))//creates a customer
+		switch (input.charAt(0))//creates a customer
 		{
 			case 'c':
 				System.out.println("Adding a Customer.\nCustomer name: ");
@@ -78,11 +96,15 @@ public class mainClass {
 	private static void Index(Scanner in)
 	{
 		String input = in.next();
-		switch(input.charAt(0))
+		switch (input.charAt(0))
 		{
 			case 'p':
-				//TODO index based on name OR genre OR both
-				for(Product p : StoreController.FindProduct(new HashMap<String, String>()))
+				System.out.println("Indexing Products: \n");
+				System.out.println("Title (Leave blank to get all): ");
+				String title = in.nextLine();
+				System.out.println("Type (Leave blank to get all): ");
+				String type = in.nextLine();
+				for (Product p : StoreController.FindProduct(title, type))
 				{
 					System.out.println(p);
 				}
